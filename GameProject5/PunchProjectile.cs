52638969e029ac0f8aad9c482e8a543c79ca96de
Darius Tyson <dariust@ksu.edu>
2023-11-22 18:03:23 -0600

    working on third level

--- GameProject5/PunchProjectile.cs
+++ GameProject5/PunchProjectile.cs
@@ -112,7 +112,7 @@ namespace GameProject5
                 }
 
             }
-            var source = new Rectangle(_animationFrame * 250, (int)projState * 512, 268, 480);
+            var source = new Rectangle(_animationFrame * 250, (int)projState * 512, 268, 440);
             spriteBatch.Draw(_texture, _position, source, Color.White, 0f, new Vector2(80, 120), 0.5f, spriteEffects, 0);
         }
     }
